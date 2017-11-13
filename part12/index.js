f9a071057a5211ec20ce706fde989870d151e1ac
Jack Franklin <jack@jackfranklin.net>
2017-11-13 20:38:10 +0000

    Set up part 12

---
+++ part12/index.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { render } from 'react-dom'
+
+import { createStore } from 'redux'
+
+const reducer = (state = { count: 0 }, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return { count: state.count + 1 }
+    default:
+      return state
+  }
+}
+
+const runReduxStore = () => {
+  const store = createStore(reducer)
+
+  console.log(store.getState())
+
+  store.dispatch({ type: 'INCREMENT' })
+
+  console.log(store.getState())
+
+  // EXERCISE: make this work
+  store.dispatch({ type: 'DECREMENT' })
+
+  console.log(store.getState())
+
+  // EXERCISE: make this work!
+  store.dispatch({ type: 'DECREMENT_BY', amount: 5 })
+  console.log(store.getState())
+}
+
+render(
+  <p>Look in the console for this one!</p>,
+  document.getElementById('react-root')
+)
+
+runReduxStore()
