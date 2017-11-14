6578c5feeaeee1b9fc138305eb384fac2345deb2
Jack Franklin <jack@jackfranklin.net>
2017-11-14 18:39:08 +0000

    Part 13

---
+++ part13/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { render } from 'react-dom'
+
+import store from './store'
+
+import { Provider } from 'react-redux'
+import App from './app'
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('react-root')
+)
---
+++ part13/store.js
@@ -0,0 +1,14 @@
+import { createStore } from 'redux'
+
+const reducer = (state = { count: 0 }, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return { count: state.count + 1 }
+    default:
+      return state
+  }
+}
+
+const store = createStore(reducer)
+
+export default store
